e9e5972002cf37f504e0c1d805cc541c60ed89df
DaisyRios <57357437+DaisyRios@users.noreply.github.com>
2023-08-17 18:37:37 -0300

    Add files via upload

---
+++ PooRepaso/SimpleProjectMaven/src/main/java/models/Main.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Producto producto1 = new Producto(1,null,null,null,null);
+		
+		System.out.println(producto1.toString());
+
+	}
+
+}
